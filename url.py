d6278645facc01b32520b9044406255b40d409ea
zll600 <3400692417@qq.com>
2024-10-30 21:39:45 +0800

    print layout

--- url.py
+++ url.py
@@ -1,4 +1,3 @@
-import tkinter
 import socket
 import ssl
 
@@ -45,8 +44,6 @@ class URL:
         s.close()
         return content
 
-WIDTH, HEIGHT = 800, 600
-
 def show(body):
     in_tag = False
     for c in body:
@@ -57,65 +54,13 @@ def show(body):
         elif not in_tag:
             print(c, end="")
 
-def lex(body):
-    text = ""
-    in_tag = False
-    for c in body:
-        if c == '<':
-            in_tag = True
-        elif c == '>':
-            in_tag = False
-        elif not in_tag:
-            text += c
-    return text
 
-HSTEP, VSTEP = 13, 18
-SCROLL_STEP = 100
+def load(url):
+    body = url.request()
+    show(body)
 
-def layout(text):
-    display_list = []
-    cursor_x, cursor_y = HSTEP, VSTEP
-    for c in text:
-        if cursor_x >= WIDTH - HSTEP:
-            cursor_y += VSTEP
-            cursor_x = HSTEP
-        display_list.append((cursor_x, cursor_y, c))
-        cursor_x += HSTEP
-    return display_list
-
-class Browser:
-    def __init__(self):
-        self.window = tkinter.Tk()
-        self.canvas = tkinter.Canvas(
-            self.window, 
-            width=WIDTH,
-            height=HEIGHT
-        )
-        self.canvas.pack()
-        self.scroll = 0
-        self.window.bind("<Down>", self.scrolldown)
-    
-
-    def scrolldown(self, e):
-        self.scroll += SCROLL_STEP
-        self.draw()
-    
-    def draw(self):
-        self.canvas.delete('all')
-        for x, y, c in self.display_list:
-            if y > self.scroll + HEIGHT:
-                continue
-            if y + VSTEP < self.scroll:
-                continue
-            self.canvas.create_text(x, y - self.scroll, text=c)
-
-    def load(self, url):
-        body = url.request()
-        text = lex(body)
-        self.display_list = layout(text)
-        self.draw()
 
 if __name__ == "__main__":
     import sys
-    Browser().load(URL(sys.argv[1]))
-    tkinter.mainloop()
+    load(URL(sys.argv[1]))
+
